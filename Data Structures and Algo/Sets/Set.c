44e7f61dea7be4abece139aea455e1a58fec16ae
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-09-06 21:56:14 +0330

    Impl Diff and Intersection

--- Data Structures and Algo/Sets/Set.c
+++ Data Structures and Algo/Sets/Set.c
@@ -14,10 +14,10 @@ struct Set *Create(int A[], int length)
     set->size = length;
     set->length = length;
     set->A = (int *)malloc(set->size * sizeof(int));
+
     for (int i = 0; i < length; i++)
-    {
         set->A[i] = A[i];
-    }
+
     return set;
 }
 
@@ -28,32 +28,83 @@ void Display(const struct Set *arr)
     printf("\n");
 }
 
-struct Set *Union(const struct Set *arr1, const struct Set *arr2)
+struct Set *Union(const struct Set *s1, const struct Set *s2)
 {
     int i = 0, j = 0, k = 0;
-    struct Set *arr3 = (struct Set *)malloc(sizeof(struct Set));
-    arr3->size = arr1->length + arr2->length;
-    arr3->A = (int *)malloc(arr3->size * sizeof(int));
+    struct Set *s3 = (struct Set *)malloc(sizeof(struct Set));
+    s3->size = s1->length + s2->length;
+    s3->A = (int *)malloc(s3->size * sizeof(int));
 
-    while (i < arr1->length && j < arr2->length)
+    while (i < s1->length && j < s2->length)
     {
-        if (arr1->A[i] < arr2->A[j])
-            arr3->A[k++] = arr1->A[i++];
-        else if (arr2->A[j] < arr1->A[i])
-            arr3->A[k++] = arr2->A[j++];
+        if (s1->A[i] < s2->A[j])
+            s3->A[k++] = s1->A[i++];
+        else if (s2->A[j] < s1->A[i])
+            s3->A[k++] = s2->A[j++];
         else
         {
-            arr3->A[k++] = arr1->A[i++];
+            s3->A[k++] = s1->A[i++];
             j++;
         }
     }
-    while (i < arr1->length)
-        arr3->A[k++] = arr1->A[i++];
-    while (j < arr2->length)
-        arr3->A[k++] = arr2->A[j++];
+    while (i < s1->length)
+        s3->A[k++] = s1->A[i++];
+    while (j < s2->length)
+        s3->A[k++] = s2->A[j++];
 
-    arr3->length = k;
-    return arr3;
+    s3->length = k;
+    return s3;
+}
+
+struct Set *Intersection(const struct Set *s1, const struct Set *s2)
+{
+    int i = 0, j = 0, k = 0;
+    struct Set *s3 = (struct Set *)malloc(sizeof(struct Set));
+    s3->size = s1->length < s2->length ? s1->length : s2->length;
+    s3->A = (int *)malloc(s3->size * sizeof(int));
+
+    while (i < s1->length && j < s2->length)
+    {
+        if (s1->A[i] < s2->A[j])
+            i++;
+        else if (s2->A[j] < s1->A[i])
+            j++;
+        else
+        {
+            s3->A[k++] = s1->A[i++];
+            j++;
+        }
+    }
+
+    s3->length = k;
+    return s3;
+}
+
+struct Set *Difference(const struct Set *s1, const struct Set *s2)
+{
+    int i = 0, j = 0, k = 0;
+    struct Set *s3 = (struct Set *)malloc(sizeof(struct Set));
+    s3->size = s1->length;
+    s3->A = (int *)malloc(s3->size * sizeof(int));
+
+    while (i < s1->length && j < s2->length)
+    {
+        if (s1->A[i] < s2->A[j])
+            s3->A[k++] = s1->A[i++];
+        else if (s2->A[j] < s1->A[i])
+            j++;
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < s1->length)
+        s3->A[k++] = s1->A[i++];
+
+    s3->length = k;
+    return s3;
 }
 
 int main()
@@ -61,14 +112,23 @@ int main()
     int A1[] = {2, 9, 21, 28, 35};
     int A2[] = {2, 3, 9, 18, 28};
 
-    struct Set *arr1 = Create(A1, sizeof(A1) / sizeof(A1[0]));
-    struct Set *arr2 = Create(A2, sizeof(A2) / sizeof(A2[0]));
+    struct Set *s1 = Create(A1, sizeof(A1) / sizeof(A1[0]));
+    struct Set *s2 = Create(A2, sizeof(A2) / sizeof(A2[0]));
 
-    Display(arr1);
-    Display(arr2);
+    Display(s1);
+    Display(s2);
 
-    struct Set *arr3 = Union(arr1, arr2);
-    Display(arr3);
+    struct Set *s3 = Union(s1, s2);
+    printf("Union: ");
+    Display(s3);
+
+    struct Set *s4 = Intersection(s1, s2);
+    printf("Intersection: ");
+    Display(s4);
+
+    struct Set *s5 = Difference(s1, s2);
+    printf("Difference: ");
+    Display(s5);
 
     return 0;
 }
