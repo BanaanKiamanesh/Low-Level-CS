816790b39e7b1fa8f4ec8d9429a7020ea4f6b637
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-09-15 22:49:40 +0330

    Heap Sort in C n CPP

---
+++ Data Structures and Algo/Sorting/HeapSort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void Swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void Heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        Swap(&arr[i], &arr[largest]);
+        Heapify(arr, n, largest);
+    }
+}
+
+void HeapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        Heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        Swap(&arr[0], &arr[i]);
+        Heapify(arr, i, 0);
+    }
+}
+
+void PrintArray(int arr[], int n)
+{
+    printf("[");
+    for (int i = 0; i < n; ++i)
+        printf((i == (n - 1)) ? "%d" : "%d, ", arr[i]);
+    printf("]\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    PrintArray(arr, n);
+
+    HeapSort(arr, n);
+
+    printf("Sorted array: ");
+    PrintArray(arr, n);
+
+    return 0;
+}
