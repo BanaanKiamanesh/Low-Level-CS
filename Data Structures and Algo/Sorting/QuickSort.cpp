cf92dcd138aa6ca4b26482bcbb698b3cf42143f2
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-09-16 19:37:50 +0330

    Quick Sort C Cpp

---
+++ Data Structures and Algo/Sorting/QuickSort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int Partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void QuickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = Partition(arr, low, high);
+        QuickSort(arr, low, pi - 1);
+        QuickSort(arr, pi + 1, high);
+    }
+}
+
+void PrintArray(int arr[], int n)
+{
+    cout << "[";
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << ((i == n - 1) ? "" : ", ");
+    cout << "]" << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 80, 30, 90, 40, 50, 70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    QuickSort(arr, 0, n - 1);
+    PrintArray(arr, n);
+    return 0;
+}
